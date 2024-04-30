e3dcd0e9bb82da1e5173ae5e8e05f42d3897288a
Ali Mianaei <ali.mianaei@gmail.com>
2024-04-30 15:25:00 +0200

    implement contact edit page

--- src/components/Contacts/EditContact.jsx
+++ src/components/Contacts/EditContact.jsx
@@ -1,7 +1,212 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+import { getAllGroups, getContact, updateContact } from "../../services/contactService";
+import { COMMENT, ORANGE, PURPLE } from "../../helpers/colors";
+import Spinner from "../Spinner";
+
 const EditContact = () => {
+  const { contactId } = useParams();
+  const navigate = useNavigate();
+
+  const [state, setState] = useState({
+    loading: false,
+    contact: {
+      fullname: "",
+      photo: "",
+      mobile: "",
+      email: "",
+      job: "",
+      group: "",
+    },
+    groups: [],
+  });
+
+  const setContactInfo = (event) => {
+    setState(prevState => ({
+      ...prevState,
+      contact: {
+        ...prevState.contact,
+        [event.target.name]: event.target.value,
+      }
+    }));
+  }
+
+  const submitForm = async (event) => {
+    event.preventDefault();
+    try {
+      setState(prevState => ({...prevState, loading: true}));
+      const { data } = await updateContact(state.contact, contactId);
+      if (data) {
+        navigate("/contacts");
+      }
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setState(prevState => ({...prevState, loading: false}));
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setState(prevState => ({...prevState, loading: true}))
+        const { data: contactData } = await getContact(contactId);
+        const { data: groupsData } = await getAllGroups();
+
+        setState(prevState => ({
+          ...prevState,
+          contact: contactData,
+          groups: groupsData
+        }));
+        
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setState(prevState => ({ ...prevState, loading: false }));
+      }
+    };
+
+    fetchData();
+  }, [contactId]);
+
+  const { loading, contact, groups } = state;
+
   return (
-    <div>Edit Contact</div>
-  )
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <section className="p-3">
+            <div className="container">
+              <div className="row my-2">
+                <div className="col text-center">
+                  <p className="h4 fw-bold" style={{ color: ORANGE }}>
+                    ویرایش مخاطب
+                  </p>
+                </div>
+              </div>
+              <hr style={{ backgroundColor: ORANGE }} />
+              <div
+                className="row p-2 w-75 mx-auto align-items-center"
+                style={{ backgroundColor: "#44475a", borderRadius: "1em" }}
+              >
+                <div className="col-md-8">
+                  <form onSubmit={submitForm}>
+                    <div className="mb-2">
+                      <input
+                        name="fullname"
+                        type="text"
+                        className="form-control"
+                        value={contact.fullname}
+                        onChange={setContactInfo}
+                        required={true}
+                        placeholder="نام و نام خانوادگی"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        name="photo"
+                        type="text"
+                        value={contact.photo}
+                        onChange={setContactInfo}
+                        className="form-control"
+                        required={false}
+                        placeholder="آدرس تصویر"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        name="mobile"
+                        type="number"
+                        className="form-control"
+                        value={contact.mobile}
+                        onChange={setContactInfo}
+                        required={true}
+                        placeholder="شماره موبایل"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        name="email"
+                        type="email"
+                        className="form-control"
+                        value={contact.email}
+                        onChange={setContactInfo}
+                        required={true}
+                        placeholder="آدرس ایمیل"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        name="job"
+                        type="text"
+                        className="form-control"
+                        value={contact.job}
+                        onChange={setContactInfo}
+                        required={true}
+                        placeholder="شغل"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <select
+                        name="group"
+                        value={contact.group}
+                        onChange={setContactInfo}
+                        required={true}
+                        className="form-control"
+                      >
+                        <option value="">انتخاب گروه</option>
+                        {groups.length > 0 &&
+                          groups.map((group) => (
+                            <option key={group.id} value={group.id}>
+                              {group.name}
+                            </option>
+                          ))}
+                      </select>
+                    </div>
+                    <div className="mt-3 text-end">
+                      <input
+                        type="submit"
+                        className="btn"
+                        style={{ backgroundColor: PURPLE }}
+                        value="ویرایش مخاطب"
+                      />
+                      <Link
+                        to={"/contacts"}
+                        className="btn mx-2"
+                        style={{ backgroundColor: COMMENT }}
+                      >
+                        انصراف
+                      </Link>
+                    </div>
+                  </form>
+                </div>
+                <div className="col-md-4">
+                  <img
+                    src={contact.photo || "https://placehold.co/300x300"}
+                    alt=""
+                    className="img-fluid rounded"
+                    style={{ border: `1px solid ${PURPLE}` }}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="text-center mt-3">
+              <img
+                src={require("../../assets/man-taking-note.png")}
+                alt=""
+                height="300px"
+                style={{ opacity: "60%" }}
+              />
+            </div>
+          </section>
+        </>
+      )}
+    </>
+  );
 }
 
 export default EditContact;
